eb48e930e93e0c3dfeafc8bc589fabb76967ba29
Anurag Tiwari <anurag.tiwari@EITMAC094.local>
2020-10-07 20:47:35 +0530

    Updates

---
+++ precache-manifest.168954d6aa6be63ec75f9c89d1175bc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd70c8b246a93a74bb838ca17976dc4",
+    "url": "/tomato_web/index.html"
+  },
+  {
+    "revision": "08935b0fe72495c77d52",
+    "url": "/tomato_web/static/css/main.b5562c93.chunk.css"
+  },
+  {
+    "revision": "d84bcadde141a24806a7",
+    "url": "/tomato_web/static/js/2.a28e2b75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tomato_web/static/js/2.a28e2b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08935b0fe72495c77d52",
+    "url": "/tomato_web/static/js/main.ad2780bc.chunk.js"
+  },
+  {
+    "revision": "6f741493ca0284b694df",
+    "url": "/tomato_web/static/js/runtime-main.511a053a.js"
+  }
+]);
